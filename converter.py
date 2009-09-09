c20fa20ef92b3ced6a835c990214ca2c04b69a37
Brian Riley <brian@btriley.com>
2009-09-08 23:48:17 -0700

    Changed URL fetching to urllib2; added error catching

--- converter.py
+++ converter.py
@@ -1,16 +1,25 @@
-import urllib
-import csv
-from cStringIO import StringIO
+import urllib2
 
 URL = 'http://finance.yahoo.com/d/quotes.csv?e=.csv&f=sl1d1t1&s={from_curr}{to_curr}=X'
 
+def _get_data(url):
+    request = urllib2.Request(url, None, {'Accept-encoding': '*'})
+    try:
+        response = urllib2.urlopen(request)
+    except urllib2.URLError:
+        return None
+    result = response.read()
+    return result
+
 def convert(from_curr, to_curr='USD', amt=1.0):
-    if from_curr == to_curr:
+    if from_curr.lower() == to_curr.lower():
         return amt
-    response = urllib.urlopen(URL.format(from_curr=from_curr, to_curr=to_curr)).read()
-    buff = StringIO()
-    buff.write(response)
     
-    curr_reader = csv.reader([buff.getvalue()])
-    exchange = curr_reader.next()
-    return float(exchange[1]) * amt
+    data = _get_data(URL.format(from_curr=from_curr, to_curr=to_curr))
+    if data:
+        exchange = data.split(',')
+        try:
+            return float(exchange[1]) * amt
+        except (IndexError, ValueError):
+            pass
+    return 0.0
--- tests.py
+++ tests.py
@@ -3,9 +3,18 @@ import converter
 
 class ConverterTestCase(unittest.TestCase):
     
-    def test_converter(self):
-        usd = converter.convert('usd', 'usd', 1.00)
+    def test_usd(self):
+        usd = converter.convert('usd', 'USD', 1.00)
         self.assertEquals(usd, 1.0)
         
+    def test_jpy(self):
         jpy = converter.convert('usd', 'jpy', 1.00)
         self.assertEquals(type(1.0), type(jpy))
+        self.assertNotEquals(jpy, 0.0)
+    
+    def test_dummy_currency(self):
+        foo = converter.convert('usd', 'foo', 1.00)
+        self.assertEquals(foo, 0.0)
+
+if __name__ == '__main__':
+    unittest.main()
